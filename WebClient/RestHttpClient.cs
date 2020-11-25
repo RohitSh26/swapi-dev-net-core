f4420a7d05d0aa56e3ff6efb71f198e775b35127
Rohit Sharma <rohit.sharma1048@gmail.com>
2020-11-25 17:45:47 -0500

    add simplified http get client using GetStringAsync method

--- WebClient/RestHttpClient.cs
+++ WebClient/RestHttpClient.cs
@@ -7,6 +7,31 @@ namespace swapi_dev_net_core.WebClient
     public class RestHttpClient
     {
         // create an asyncronus http client
-        
+        public static async Task<T> GetClient<T>(string url, Func<string, T> callback)
+        {
+            // create an HttpClient
+            try
+            {
+                using(var client = new HttpClient())
+                {
+                    // simple approach 
+                    var body = await client.GetStringAsync(url);
+
+                    if(body == null)
+                    {
+                        return default;
+                    }
+                    else
+                    {
+                        return callback(body);
+                    }                    
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+                return default;
+            }
+        }
     }
 }
