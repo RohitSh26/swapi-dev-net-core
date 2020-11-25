69542a77c46db6e2e000088d43489894ea41a18a
Rohit Sharma <rohit.sharma1048@gmail.com>
2020-11-25 17:44:18 -0500

    add async http get client

--- WebClient/RestClient.cs
+++ WebClient/RestClient.cs
@@ -7,6 +7,38 @@ namespace swapi_dev_net_core.WebClient
     public class RestClient
     {
         // create a HTTP Client make get request, make an asyncronous call
-        
+        public static async Task<T> GetClient<T>(string url, Func<string, T> callback)
+        {
+            try
+            {
+                using(var client = new HttpClient())
+                {
+                    // make an asyncronous call, get response body
+                    using(var response = await client.GetAsync(url))
+                    {
+                        // make sure that response code is success
+                        response.EnsureSuccessStatusCode();
+                        var body = await response.Content.ReadAsStringAsync();
+
+                        // check if we get non null response body
+                        if(body == null)
+                        {
+                            // return the "default" or "empty" value 
+                            return default;
+                        }
+                        else 
+                        {
+                            return callback(body);
+                        }
+                    }
+                }
+            }
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+                // return the "default" or "empty" value 
+                return default;
+            }
+        }
     }
 }
